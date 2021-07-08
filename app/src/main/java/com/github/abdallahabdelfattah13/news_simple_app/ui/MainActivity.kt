200d1a08e6d34690de01fae0bfdf5f49a22ef76f
AbdAllah Abd-El-Fattah <abdallah.a.elfatah@gmail.com>
2021-07-08 16:03:25 +0200

    init commit

---
+++ app/src/main/java/com/github/abdallahabdelfattah13/news_simple_app/ui/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.github.abdallahabdelfattah13.news_simple_app.ui
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.github.abdallahabdelfattah13.news_simple_app.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
