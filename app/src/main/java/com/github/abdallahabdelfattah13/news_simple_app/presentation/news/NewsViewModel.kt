41b51eeb36f4e055b21eb88ddbcf0da739a3f879
AbdAllah Abd-El-Fattah <abdallah.a.elfatah@gmail.com>
2021-07-09 17:46:09 +0200

    not sure if it's working yet..

--- app/src/main/java/com/github/abdallahabdelfattah13/news_simple_app/presentation/news/NewsViewModel.kt
+++ app/src/main/java/com/github/abdallahabdelfattah13/news_simple_app/presentation/news/NewsViewModel.kt
@@ -1,33 +1,44 @@
 package com.github.abdallahabdelfattah13.news_simple_app.presentation.news
 
 import androidx.lifecycle.LiveData
-import androidx.lifecycle.LiveDataReactiveStreams
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
 import androidx.paging.PagingData
+import androidx.paging.rxjava2.cachedIn
 import com.github.abdallahabdelfattah13.news_simple_app.data.models.Article
 import com.github.abdallahabdelfattah13.news_simple_app.data.news.NewsRepository
-import io.reactivex.BackpressureStrategy
-import io.reactivex.Flowable
 import io.reactivex.Scheduler
-import io.reactivex.subjects.PublishSubject
-import io.reactivex.subjects.Subject
-import java.util.concurrent.TimeUnit
+import io.reactivex.android.schedulers.AndroidSchedulers
+import io.reactivex.disposables.CompositeDisposable
+import kotlinx.coroutines.ExperimentalCoroutinesApi
 
 class NewsViewModel(
     private val newsRepository: NewsRepository,
     private val backgroundScheduler: Scheduler
 ) : ViewModel() {
 
-    val searchQueryStream: Subject<String> by lazy {
-        PublishSubject.create()
+    private val _state = MutableLiveData<PagingData<Article>>()
+    val state: LiveData<PagingData<Article>>
+        get() = _state
+
+    private val compositeDisposables = CompositeDisposable()
+
+    @ExperimentalCoroutinesApi
+    fun searchForQ(q: String) {
+        val x = newsRepository
+            .searchNews(q)
+            .cachedIn(viewModelScope)
+            .subscribeOn(backgroundScheduler)
+            .observeOn(AndroidSchedulers.mainThread())
+            .subscribe {
+                _state.value = it
+            }
     }
 
-    val data: LiveData<PagingData<Article>> by lazy {
-        val source: Flowable<PagingData<Article>> = searchQueryStream
-            .toFlowable(BackpressureStrategy.DROP)
-            .flatMap { newsRepository.searchNews(it) }
-
-        LiveDataReactiveStreams.fromPublisher(source)
+    override fun onCleared() {
+        super.onCleared()
+        compositeDisposables.dispose()
     }
 
 }
